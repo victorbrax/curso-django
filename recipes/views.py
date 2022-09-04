6d1f9802f14c77f9596d19a4ea44f0ce24406f8f
Víctor Gomes <joaogomespcl@gmail.com>
2022-09-04 00:55:29 -0300

    urls reorder

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,14 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+# from django.shortcuts import render
 
 
 def home(request):
     return HttpResponse('HOME 1')
 
+
 def contato(request):
     return HttpResponse('contato')
+
+
+def sobre(request):
+    return HttpResponse('sobre')
